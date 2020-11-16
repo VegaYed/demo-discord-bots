389c602ac58618acc75078966218494f6a95bef2
Osvaldo <osvaldoleivabarria1@gmail.com>
2020-11-16 00:01:04 -0300

    test bot

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+const { Client, MessageEmbed } = require('discord.js');
+const client = new Client();
+
+client.on('ready', ()=>{
+    console.log(`Bot is ready as! ${client.user.tag}`);
+})
+
+client.on('message', async message => {
+    if(message.content == '!ping'){
+        message.reply('PONG!');
+    }
+    if(message.content == '!embed'){
+        const embed = new MessageEmbed()
+            .setTitle('A sick little embed')
+            .setColor(0xff0000)
+            .setDescription('Hello this is a sick embed!');
+        message.reply(embed);
+    }
+    if(message.content == "!clear"){
+        const fetched = await message.channel.fetchMessages({limit: 100});
+        message.channel.bulkDelete(fetched);
+    }
+})
+
+client.login('Nzc3NjM5NDMyNjU1ODYzODQ5.X7GXKA.xwzbVmNcJ3AsJSSQx9DxH4WoFec');
