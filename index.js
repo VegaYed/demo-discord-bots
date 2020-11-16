384a8032da899f68c048addc010bbc3e96228b4a
Osvaldo <osvaldoleivabarria1@gmail.com>
2020-11-16 00:16:37 -0300

    renovacion token

--- index.js
+++ index.js
@@ -23,4 +23,4 @@ client.on('message', async message => {
     }
 })
 
-client.login('Nzc3NjM5NDMyNjU1ODYzODQ5.X7GXKA.xwzbVmNcJ3AsJSSQx9DxH4WoFec');
+client.login('Nzc3NjM5NDMyNjU1ODYzODQ5.X7GXKA.OwaqYO3cWaNQpCEPv4p1e537sJA');
